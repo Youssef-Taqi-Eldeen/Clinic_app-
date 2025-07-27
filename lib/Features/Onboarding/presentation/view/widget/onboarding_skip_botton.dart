1cb1e56aa26db3e31695a99afd872ae2f0228feb
Youssef Taqi Eldeen <taqieldeenyoussef@gmail.com>
2025-07-27 04:24:41 +0300

    Enable skip button navigation in login

--- lib/Features/Onboarding/presentation/view/widget/onboarding_skip_botton.dart
+++ lib/Features/Onboarding/presentation/view/widget/onboarding_skip_botton.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:y_clinic_app/core/utils/app_router.dart';
 
 class OnboardingSkipButton extends StatelessWidget {
   const OnboardingSkipButton({
@@ -8,7 +10,9 @@ class OnboardingSkipButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: () {},
+      onPressed: () {
+        context.go(AppRouter.kLOginone);
+      },
       child: const Text(
         "SKip",
         style: TextStyle(
